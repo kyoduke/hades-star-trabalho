844effba92421973a7dd0eeb3ef5d2ecd1250faf
Kyoduke <caioduque.dev@gmail.com>
2024-06-10 14:26:04 -0300

    add color event to title

--- js/script.js
+++ js/script.js
@@ -1,4 +1,19 @@
 const text_elements = document.querySelectorAll('.text');
+const title = document.querySelector('h1');
+const header = document.querySelector('header');
+
+title.addEventListener('click', event => {
+  const color = header.style.backgroundColor;
+  console.log(color);
+  if(color == 'rgb(64, 40, 128)') {
+    header.style.backgroundColor = '#f4f4f4';
+    header.style.color = 'black';
+  } else {
+    header.style.backgroundColor = '#402880';
+    header.style.color = 'white';
+  }
+});
+
 text_elements.forEach(element => {
   element.addEventListener('mouseover', event => {
     element.style.backgroundColor = '#402880';
@@ -9,3 +24,5 @@ text_elements.forEach(element => {
     element.style.color = 'black';
   });
 });
+
+
